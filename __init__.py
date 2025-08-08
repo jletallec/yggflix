1c57fb8226951e8bd97d945df068a4e794046cdc
Josselin LE TALLEC <josselin.letallec@proton.me>
2025-08-08 12:32:44 +0200

    First Commit

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from yggflix_result import YggflixResult
+from yggflix_service import YggflixService
+from yggflix_api import YggflixAPI
+
+__all__ = ["YggflixResult", "YggflixService", "YggflixAPI"]
