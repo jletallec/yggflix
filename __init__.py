4da16ed6a25763b33f88b9478a72e68680c65f22
Josselin LE TALLEC <josselin.letallec@proton.me>
2025-08-08 12:51:50 +0200

    Organisation des Fichiers + README

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from yggflix_result import YggflixResult
-from yggflix_service import YggflixService
-from yggflix_api import YggflixAPI
-
-__all__ = ["YggflixResult", "YggflixService", "YggflixAPI"]
